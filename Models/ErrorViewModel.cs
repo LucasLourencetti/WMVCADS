72d4dfd6cafa664b189e25196753c1a4b65433d8
llour <llour@NOT_LOUREN7>
2023-08-22 19:41:27 -0300

    Adicionar arquivos de projeto.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WMVCADS.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
